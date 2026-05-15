21507eef285ba48e386e047c409e2043018e0e99
Ghost99X-tech <gangghost432@gmail.com>
2026-05-15 18:37:36 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "owner",
     version: "1.3.0",
-    author: "亗•𝘔𝘈𝘔𝘜𝘕✿᭄",
+    author: "亗•𝘔𝘈𝘔𝘜𝘕✿᭄+ edit by Milon",
     role: 0,
     shortDescription: "Owner information with image",
     category: "Information",
@@ -18,17 +18,17 @@ module.exports = {
   onStart: async function ({ api, event }) {
     const ownerText = 
 `╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : 亗•𝘔𝘈𝘔𝘜𝘕✿᭄
-│🧸 Nɪᴄᴋ       : Vondo
-│ 🎂 Aɢᴇ        : 19+
-│ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
-│ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
-│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 2ɴᴅ Yᴇᴀʀ
-│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 🄺🄷🅄🄻🄽🄷  
+│ 👤 Nᴀᴍᴇ       : MD MILON SARKAR
+│🧸 Nɪᴄᴋ       : Milon
+│ 🎂 Aɢᴇ        : 23+
+│ 💘 Rᴇʟᴀᴛɪᴏɴ : Pure Sɪɴɢʟᴇ
+│ 🎓 Pʀᴏғᴇssɪᴏɴ : Job (private company)
+│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Dakhil (ssc2020)
+│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : Kurigram, Bangladesh 
 ├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : https://www.facebook.com/share/171Y441F7H/
-│ 💬 Messenger: m.me/61558931578859
-│ 📞 WhatsApp  : wa.me/01830981279
+│ 📘 Facebook  : https://www.facebook.com/share/1CNLskKAtw/
+│ 💬 Messenger: m.me/100081225144815
+│ 📞 WhatsApp  : wa.me/016*****314
 ╰────────────────╯`;
 
     const cacheDir = path.join(__dirname, "cache");
