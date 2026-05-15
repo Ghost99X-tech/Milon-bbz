817ce12b3dbf7e3f31996c37e0b40964022f704a
Ghost99X-tech <gangghost432@gmail.com>
2026-05-15 18:32:45 +0600

    Update and rename mamun.js to ownerinfo.js

--- scripts/cmds/ownerinfo.js
+++ scripts/cmds/ownerinfo.js
@@ -1,13 +1,13 @@
 module.exports = {
   config: {
-    name: "mamun",
+    name: "owner info",
     version: "1.0.4",
-    author: "〲MAMUNツ࿐ T.T　o.O",
+    author: "Milon",
     role: 0,
-    shortDescription: "Mamun Profile (No Image)",
+    shortDescription: "Milon Profile (No Image)",
     category: "Information",
     guide: {
-      en: "type mamun"
+      en: "type owner info"
     }
   },
 
@@ -21,19 +21,15 @@ module.exports = {
 `╔════════════════════════════════════════╗
 ║ 🌌✨ 𝗣𝗥𝗢𝗙𝗜𝗟𝗘 𝗖𝗔𝗥𝗗 ✨🌌 ║
 ╠════════════════════════════════════════╣
-║ 🌟 Name       : 〲MAMUNツ࿐ T.T　o.O
-║ 🎂 Age        : 20
-║ 🆔 FF ID      : 3246615019
-║ 💌 Status     : single
-║ 📍 Location   : Khulna, Bangladesh
-║ 🎮 Hobbies    : Gaming 🎮 | Coding 💻 | Music 🎧
+║ 🌟 Name       : MD MILON SARKAR
+║ 🎂 Age        : 23+
+║ 💌 Status     :pure single
+║ 📍 Location   : Kurigram, Bangladesh
+║ 🎮 Hobbies    : phone | Coding 💻 |  grouping 
 ╠════════════════════════════════════════╣
 ║ 🔗 Social Links:
-║ • Facebook  : https://www.facebook.com/profile.php?id=61589546816594
-║ • WhatsApp  : 01830981279
-║ • TikTok    : ma.m.un97
-╠════════════════════════════════════════╣
-║ 🎯 Game      : Free Fire 🔫
+║ • Facebook  : https://www.facebook.com/share/1BYq7qn8Rz/
+║ • WhatsApp  : 016******14
 ╚════════════════════════════════════════╝`;
 
     api.sendMessage(profileText, event.threadID, event.messageID);
