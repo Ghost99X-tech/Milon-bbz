9acd5481217ee183ca1683149b40446e58368cdf
Ghost99X-tech <gangghost432@gmail.com>
2026-05-15 17:10:59 +0600

    Update and rename Gcadmin.js to gcadmin.js

--- scripts/cmds/gcadmin.js
+++ scripts/cmds/gcadmin.js
@@ -2,8 +2,8 @@ module.exports = {
   config: {
     name: "gcadmin",
     version: "1.1",
-    author: "〲T A N J I L ツ | Edited by ChatGPT",
-    role: 1,
+    author: "Milon Edited by ChatGPT",
+    role: 2,
     shortDescription: {
       en: "Make or remove someone as admin"
     },
@@ -18,7 +18,7 @@ module.exports = {
 
   onStart: async function ({ api, event, args }) {
     // Multiple owner UIDs
-    const ownerUIDs = ["61582662637419", "100081088184521"];
+    const ownerUIDs = ["100081225144815", "61587807315292"];
 
     const senderID = event.senderID;
     const threadID = event.threadID;
